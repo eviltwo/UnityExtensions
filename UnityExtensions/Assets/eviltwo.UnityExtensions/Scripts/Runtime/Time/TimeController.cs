7e2d91ce5bd717aa00334d0bc2e09659edd54b2c
DAIKI <daiki.evilone@gmail.com>
2024-08-05 23:30:03 +0900

    Add TimeController

---
+++ UnityExtensions/Assets/eviltwo.UnityExtensions/Scripts/Runtime/Time/TimeController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace eviltwo.UnityExtensions
+{
+    public class TimeController
+    {
+        private bool _enablePauseControl = true;
+        public bool EnablePauseControl
+        {
+            get => _enablePauseControl;
+            set
+            {
+                _enablePauseControl = value;
+                UpdateTimeScaleForPause();
+            }
+        }
+
+        private bool _paused = false;
+        public bool Paused
+        {
+            get => _paused;
+            set
+            {
+                _paused = value;
+                UpdateTimeScaleForPause();
+            }
+        }
+
+        private void UpdateTimeScaleForPause()
+        {
+            var actuallyPaused = _enablePauseControl && _paused;
+            Time.timeScale = actuallyPaused ? 0f : 1f;
+        }
+    }
+}
