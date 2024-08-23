b0d6a25c4543808bf4dda90698540bc96a5caae7
DAIKI <daiki.evilone@gmail.com>
2024-08-23 12:57:27 +0900

    Added PlatformDataPathProvider.

---
+++ UnityExtensions/Assets/eviltwo.UnityExtensions/Scripts/Runtime/Platforms/PlatformDataPathProvider.cs
@@ -0,0 +1,48 @@
+using System.IO;
+using UnityEngine;
+
+#if STEAMWORKS
+using Steamworks;
+#endif
+
+namespace eviltwo.UnityExtensions
+{
+    /// <summary>
+    /// This class checks whether Steamworks is being used and provides the appropriate directory path for save data.
+    /// </summary>
+    public static class PlatformDataPathProvider
+    {
+        public static string RootFolderName = "SaveData";
+        public static string SteamFolderName = "Steam";
+        public static string SharedFolderName = "Shared";
+
+        public static string GetDirectory()
+        {
+#if STEAMWORKS
+            try
+            {
+                return GetSteamDirectory();
+            }
+            catch (System.Exception)
+            {
+                Debug.LogWarning("Due to an error accessing Steamworks, the shared directory will be used. Steamworks may not be initialized.");
+            }
+#endif
+
+            return GetSharedDirectory();
+        }
+
+#if STEAMWORKS
+        private static string GetSteamDirectory()
+        {
+            var userSteamId = SteamUser.GetSteamID().m_SteamID;
+            return Path.Combine(Application.persistentDataPath, RootFolderName, SteamFolderName, userSteamId.ToString());
+        }
+#endif
+
+        private static string GetSharedDirectory()
+        {
+            return Path.Combine(Application.persistentDataPath, RootFolderName, SharedFolderName);
+        }
+    }
+}
